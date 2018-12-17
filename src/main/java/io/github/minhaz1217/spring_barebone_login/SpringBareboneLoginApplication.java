217fdd2cc35f1a7b94046f9a5dbb6cbbee07804b
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2018-12-17 18:54:33 +0600

    fixed the -> Reason: Failed to determine a suitable driver class by adding a line above the main class

--- src/main/java/io/github/minhaz1217/spring_barebone_login/SpringBareboneLoginApplication.java
+++ src/main/java/io/github/minhaz1217/spring_barebone_login/SpringBareboneLoginApplication.java
@@ -1,14 +1,15 @@
 package io.github.minhaz1217.spring_barebone_login;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SpringBareboneLoginApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBareboneLoginApplication.class, args);
 	}
-
 }
 
