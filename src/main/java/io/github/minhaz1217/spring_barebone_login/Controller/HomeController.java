c7e63380b125a496718432758a8b7fb18d1f0e91
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2018-12-17 18:55:20 +0600

    difference between @PathVariable and @RequestParam,

--- src/main/java/io/github/minhaz1217/spring_barebone_login/Controller/HomeController.java
+++ src/main/java/io/github/minhaz1217/spring_barebone_login/Controller/HomeController.java
@@ -1,10 +1,9 @@
 package io.github.minhaz1217.spring_barebone_login.Controller;
 
 
+import io.github.minhaz1217.spring_barebone_login.Model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -20,4 +19,22 @@ public class HomeController {
     public String getLogin2(){
         return "Login";
     }
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET, value = "/test/{user}/{pass}")
+    public String fromUsername(@PathVariable String user, @PathVariable String pass){
+        // to get values from this endpoint
+        // Use urls like this : http://localhost:28080/test/myUsername/myPassword
+        return user + "  " + pass;
+    }
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET, value = "/test2")
+    public String fromUsername2(@RequestParam(value = "user")String user, @RequestParam(value = "pass") String pass){
+        // to get values from this endpoint use it like this:
+        // http://localhost:28080/test2?user=myUsername&pass=myPassword
+
+        return user + "  " + pass;
+    }
+
+
 }
