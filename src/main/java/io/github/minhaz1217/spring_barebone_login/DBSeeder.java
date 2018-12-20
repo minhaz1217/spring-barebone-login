0f771becb162808470e4f20d8e0cd87375339017
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2018-12-21 02:26:49 +0600

    added datas to the changed tables.

--- src/main/java/io/github/minhaz1217/spring_barebone_login/DBSeeder.java
+++ src/main/java/io/github/minhaz1217/spring_barebone_login/DBSeeder.java
@@ -1,22 +1,34 @@
 package io.github.minhaz1217.spring_barebone_login;
 
+import io.github.minhaz1217.spring_barebone_login.Model.Role;
 import io.github.minhaz1217.spring_barebone_login.Model.User;
+import io.github.minhaz1217.spring_barebone_login.Repository.RoleRepository;
 import io.github.minhaz1217.spring_barebone_login.Repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.HashSet;
 
 @Component
 public class DBSeeder implements CommandLineRunner{
 
     private UserRepository userRepository;
-    DBSeeder(UserRepository userRepository){
+    private RoleRepository roleRepository;
+    DBSeeder(UserRepository userRepository, RoleRepository roleRepository){
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
     @Override
     public void run(String... args) throws Exception {
         userRepository.deleteAll();
-        userRepository.save(new User("user", "user"));
-        userRepository.save(new User("1", "1"));
-        userRepository.save(new User("admin", "admin"));
+        roleRepository.deleteAll();
+        roleRepository.save(new Role("ADMIN"));
+        roleRepository.save(new Role("USER"));
+
+        userRepository.save(new User("user", "user", "user@user.com" , true,  new HashSet<>(Arrays.asList(roleRepository.findRoleByRole("ADMIN") ) ) ));
+        userRepository.save(new User("1", "1", "1" , true,  new HashSet<>(Arrays.asList(roleRepository.findRoleByRole("ADMIN") ) ) ));
+        userRepository.save(new User("admin", "admin", "admin" , true,  new HashSet<>(Arrays.asList(roleRepository.findRoleByRole("ADMIN") ) ) ));
+
     }
 }
--- src/main/java/io/github/minhaz1217/spring_barebone_login/Repository/RoleRepository.java
+++ src/main/java/io/github/minhaz1217/spring_barebone_login/Repository/RoleRepository.java
@@ -4,5 +4,5 @@ import io.github.minhaz1217.spring_barebone_login.Model.Role;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface RoleRepository extends MongoRepository<Role, String>{
-    Role findByRole(String role);
+    Role findRoleByRole(String role);
 }
