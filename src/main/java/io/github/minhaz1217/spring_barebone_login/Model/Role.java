14859bdd12b954303d6a440786d9ad5466ebbd04
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2018-12-21 02:09:44 +0600

    changed the user class and added a role class

---
+++ src/main/java/io/github/minhaz1217/spring_barebone_login/Model/Role.java
@@ -0,0 +1,36 @@
+package io.github.minhaz1217.spring_barebone_login.Model;
+
+
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Id;
+
+@Document(collection = "role")
+public class Role {
+    @Id
+    private  String id;
+    @Indexed(unique = true, direction = IndexDirection.DESCENDING, dropDups = true)
+    private String role;
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
--- src/main/java/io/github/minhaz1217/spring_barebone_login/Model/User.java
+++ src/main/java/io/github/minhaz1217/spring_barebone_login/Model/User.java
@@ -1,11 +1,13 @@
 package io.github.minhaz1217.spring_barebone_login.Model;
 
 
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Set;
 
 @Document(collection = "User")
 public class User {
@@ -13,11 +15,19 @@ public class User {
     private String id;
     private String username;
     private String password;
+    private String email;
+    private boolean enabled;
+    @DBRef
+    private Set<Role> roles;
 
     private User(){}
-    public User(String username, String password) {
+
+    public User(String username, String password, String email, boolean enabled, Set<Role> roles) {
         this.username = username;
         this.password = password;
+        this.email = email;
+        this.enabled = enabled;
+        this.roles = roles;
     }
 
     public String getId() {
@@ -42,5 +52,29 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 }
