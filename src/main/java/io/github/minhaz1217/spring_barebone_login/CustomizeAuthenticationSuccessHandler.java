30e472fb18fb56d2a1d22a6eda991a18ab15337e
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2018-12-25 01:59:33 +0600

    added Custom Authentication Success handler, that after successful authentication redirects the user depending on the Role of that user

---
+++ src/main/java/io/github/minhaz1217/spring_barebone_login/CustomizeAuthenticationSuccessHandler.java
@@ -0,0 +1,27 @@
+package io.github.minhaz1217.spring_barebone_login;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomizeAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+        for (GrantedAuthority auth: authentication.getAuthorities()){
+            if("ADMIN".equals(auth.getAuthority())){
+                httpServletResponse.sendRedirect("/dashboard");
+            }
+        }
+
+
+
+    }
+}
